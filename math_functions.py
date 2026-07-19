ed0b85512222b565a5991f6abf204aa997c847a4
LoudMines <luitmeinen@gmail.com>
2026-07-19 18:27:27 +0200

    Added P006 and P007, implemented a first prime sieve and improved overall documentation.

--- math_functions.py
+++ math_functions.py
@@ -20,3 +20,22 @@ def is_prime(n):
         else:
             i += 1
     return True
+
+"""
+-------------------- Prime Sieves --------------------
+Used to generate all primes up to n. The most basic is Eratosthenes, 
+faster sieves will be implemented later.
+"""
+
+@jit(nopython=True)
+def sieve_eratosthenes(n):
+    # An array which holds whether a number is prime, 0 and 1 are False, after that we can index
+    # by the prime number
+    prime_mask = [False, False] + ([True] * (n - 1))
+    for prime in range(2, math.ceil(math.sqrt(n))):
+        if prime_mask[prime]:
+            j = prime ** 2
+            while j <= n:
+                prime_mask[j] = False
+                j += prime
+    return [mask_index for mask_index, prime in enumerate(prime_mask) if prime]
